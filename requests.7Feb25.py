0d59b3c259724a54512760b1ecdb48ed13295b8c
ATP <clmishto3@yahoo.com>
2025-02-09 22:59:12 +0200

    Got json dump to work

--- requests.7Feb25.py
+++ requests.7Feb25.py
@@ -1,65 +1,71 @@
 # AI boom right now - easy to get hired; internship ; framework to generate code: react native & then convert into other type of code
 # difference mobile web - distance (ex: bluetooth) -- is the mobile app really necessary
 # 3x categ site: prezentare, online stores, aplicatii web
-
+# Data management 
 
 
 # import requests
 # url = 'https://www.link-academy.com'
 # response = requests.get(url)
-# print(response)
-# print(response.status_code)
-# print(type(response.content))
-#print(response.text)
+# print(response) # OUT: <Response [200]>
+# print(response.status_code) # OUT: 200
+#print(response.content, type(response.content)) # OUT: code , type: bytes
+# print(response.text) # OUT: html
 
+# save the response.text == html into file
 # file_handler = open("link-academy2.html", "w", encoding="utf-8")
 # file_handler.write(response.text)
 # file_handler.close()
 
+# version 2, same
 # with open ("link-academy.html", "w", encoding="utf-8") as file_handler:
 #     file_handler.write(response.text)
 
 #-----------------------
 
-# import requests
-# import random
-# import json
-# import time
+import requests
+import random
+import json
+import time
 
-# api_options = ["", "-gcp", "1", "2", "3", "4"]
-# valoare = random.choice(api_options)
-# BASE_URL = f"https://api{valoare}.binance.com"
-# print(BASE_URL)
+api_options = ["", "-gcp", "1", "2", "3", "4"]
+valoare = random.choice(api_options)
+BASE_URL = f"https://api{valoare}.binance.com" ; print(BASE_URL)
+PATH = "/api/v3/avgPrice"
+URL = BASE_URL + PATH ; print(URL)
 
-# PATH = "/api/v3/avgPrice"
+x = requests.get('https://w3schools.com') ; print(x.status_code) # OUT: 200
 
-# URL = BASE_URL + PATH
-# print(URL)
+def call_api():
+    symbol = "ETHBTC"
+    response = requests.get(URL, params={'symbol':symbol}) # https://www.w3schools.com/PYTHON/ref_requests_get.asp
+#get() M returns a requests.Response object / returns an object
+#params={key: value} - dict, list of typles or bytes to SEND AS QUERY STRING
+# allow_redirect=True (default true: enable/disable redirection)
+# auth == tuple to enable HTTP authetication
+    print(response.content) # OUT: b'{"mins":5,"price":"0.02740966","closeTime":1739133789350}' -bytes
 
-# symbol = "ETHBTC"
-
-# def call_api():
-#     response = requests.get(URL, params={'symbol':symbol})
-#     print(response.content)
-
+# since an object (bytes) was returned via get(), you can now use json to deserialize
 #     # deserialize
-#     json_response = json.loads(response.content) # load vs loads - read from file fpu vs read from memory 
-#     print(json_response, type(json_response)) # OUT: type dict
-#     price = json_response['price']
-#     print("price = ", price)
+    json_response = json.loads(response.content) # load vs loads - read from file fpu vs read from memory 
+    print(json_response, type(json_response)) # OUT: type dict 
+    price = json_response['price'] ; print("price = ", price)
 
 #     with open("ethereum.txt", "a") as fwriter:
 #         fwriter.write(price)
 #         fwriter.write("\n")
-# # json.dumps
-#     # json_dumps = json.dumps(response.content)
-#     # print(json_dumps)
 
-# if __name__ == "__main__":
-#     while True:
-#         call_api()
-#         doarme =random.randint(1,3)
-#         time.sleep(doarme)
+# version 2 # only saves latest file, not everything, but work so far.
+    with open('json.txt', 'w') as file:
+        data = {symbol: json_response['price']}
+        json.dump(data, file, indent=4, default=float)
+        print(f"Price saved to json.txt")
+
+if __name__ == "__main__":
+    while True:
+        call_api()
+        doarme =random.randint(1,3)
+        time.sleep(doarme)
 
 
 # #-----------------------
@@ -72,28 +78,33 @@
 
 # ---------------
 
-import requests
-import json
-from pprint import pprint
+# import requests
+# import json
+# from pprint import pprint
 
-latitudine = 44.432384
-longitudine = 26.1063209
+# latitudine = 44.432384
+# longitudine = 26.1063209
 
-BASE_URL = "https://api.open-meteo.com"
-PATH = "/v1/forecast"
-URL = BASE_URL + PATH
+# BASE_URL = "https://api.open-meteo.com"
+# PATH = "/v1/forecast"
+# URL = BASE_URL + PATH
 
-parametri = {'latitude': latitudine, 'longitude': longitudine, 'current':'temperature_2m'}
+# parametri = {'latitude': latitudine, 'longitude': longitudine, 'current':'temperature_2m'}
 
-response = requests.get(URL, params=parametri)
-print(response.request.url)
-print(response.status_code)
-print(response.content)
+# response = requests.get(URL, params=parametri)
+# print(response.request.url)
+# print(response.status_code)
+# print(response.content)
 
-#$ curl "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+# #$ curl "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
 
-json_response = json.loads(response.content) # convert to dictionary
-print('temperature este:', json_response['current']['temperature_2m'])
+# json_response = json.loads(response.content) # convert to dictionary
+# print('temperature este:', json_response['current']['temperature_2m'])
+
+
+# ---------------
+
+
 
 
 #     json_response = json.loads(response.content) # load vs loads - read from file fpu vs read from memory 
